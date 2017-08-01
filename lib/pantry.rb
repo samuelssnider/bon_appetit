e785fd0f1da04710e32f138520823ed33019dd56
samuelssnider <sam_redins@yahoo.com>
2017-08-01 09:22:16 -0600

    forgot to commit, three more tests passing

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -5,5 +5,20 @@ class Pantry
     @stock = {}
   end
 
+  def stock_check(ingredient)
+   if @stock[ingredient]
+     @stock[ingredient]
+   else
+     0
+   end
+  end
+
+  def restock(ingredient, quantity)
+    unless @stock[ingredient]
+      @stock.merge!(ingredient => quantity)
+    else
+    end
+  end
+
 
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -6,13 +6,39 @@ class PantryTest < Minitest::Test
 
   def test_it_can_be_created
     pan = Pantry.new
+
     assert_instance_of Pantry, pan
     assert pan
   end
 
   def test_stock_can_be_got
     pan = Pantry.new
+
     assert pan.stock.empty?
   end
+
+  def test_stock_can_be_checked
+    pan = Pantry.new
+
+    assert_equal 0, pan.stock_check("Cheese")
+  end
+
+  def test_restock_can_be_called
+    pan = Pantry.new
+
+    pan.restock("Cheese", 10)
+  end
+
+  def test_restock_working
+    pan = Pantry.new
+
+    assert_equal 0, pan.stock.count
+
+    pan.restock("Cheese", 10)
+
+    assert_equal 1 , pan.stock.count
+    assert_equal 10, pan.stock_check("Cheese")
+  end
+
 
 end
