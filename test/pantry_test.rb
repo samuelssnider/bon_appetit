727122dd43d0e4cc659e39ddfda296277dd78966
samuelssnider <sam_redins@yahoo.com>
2017-08-01 09:09:35 -0600

    first two tests working

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,4 +4,15 @@ require 'minitest/pride'
 
 class PantryTest < Minitest::Test
 
+  def test_it_can_be_created
+    pan = Pantry.new
+    assert_instance_of Pantry, pan
+    assert pan
+  end
+
+  def test_stock_can_be_got
+    pan = Pantry.new
+    assert pan.stock.empty?
+  end
+
 end
