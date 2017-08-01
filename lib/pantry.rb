686400b90dd02ee4fbca8ec031f7f191ed882e0c
samuelssnider <sam_redins@yahoo.com>
2017-08-01 09:50:43 -0600

    convert_unit

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -21,5 +21,27 @@ class Pantry
     end
   end
 
+  def convert_units(recipe)
+    converted_ingreditent_units = Hash.new(0)
+    recipe.ingredients.each do |ingredient|
+      converted_ingreditent_units.merge!(ingredient[0] => convert_unit(ingredient[1]))
+    end
+    converted_ingreditent_units
+  end
+
+  def convert_unit(quantity)
+    case quantity
+    when quantity < 1.0
+      unit = "Milli-Units"
+      quantiity *= 1000.0
+    when quantity >= 1.0 && quantity <= 100.0
+      unit = "Universal Units"
+    else
+      unit = "Centi-Units"
+      quantity /= 100.0
+    end
+    {quantity: quantity, units: unit}
+  end
+
 
 end
